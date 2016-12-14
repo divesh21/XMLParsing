2fb53019f98be15c6f6ebc6e67bd655a1c6973df
student14 <student14@apples-macbook-air-4.local>
2016-12-14 11:51:25 +0530

    Initial Commit

---
+++ XML Parshing/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XML Parshing
+//
+//  Created by student14 on 14/12/16.
+//  Copyright © 2016 Felix-ITs-Divesh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
